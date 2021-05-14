0139d6023bb91509f2eacc315a74a20ba6895d0c
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2021-05-14 12:27:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -34,11 +34,32 @@ Runner.run(runner, engine);
 
 
 
-/* 
- 
-add events here
 
-*/
+//  collisionStart event on an engine
+Events.on(engine, 'collisionStart', function(event) {
+    var pairs = event.pairs;
+    
+    // change object colours to show those starting a collision
+    for (var i = 0; i < pairs.length; i++) {
+        var pair = pairs[i];
+        pair.bodyA.render.fillStyle = 'red';
+        pair.bodyB.render.fillStyle = 'red';
+    }
+    });
+
+    // an example of using collisionActive event on an engine
+    Events.on(engine, 'collisionActive', function(event) {
+        var pairs = event.pairs;
+
+        // change object colours to show those in an active collision (e.g. resting contact)
+        for (var i = 0; i < pairs.length; i++) {
+            var pair = pairs[i];
+            pair.bodyA.render.fillStyle = '#333';
+            pair.bodyB.render.fillStyle = '#333';
+        }
+    });
+
+
 
 var bodyStyle = { fillStyle: '#222' };
 
